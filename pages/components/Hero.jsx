afc56fdcda30dce7f573361291a5f2c42da06664
Samuel Pokam <dixiades@gmail.com>
2023-01-05 14:29:50 +0100

    Navbar + Hero added

---
+++ pages/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { AiFillLinkedin, AiFillGithub, AiFillMail } from "react-icons/ai";
+import Image from "next/image";
+import deved from "../../public/dev-ed-wave.png";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="text-center p-10">
+        <h2 className=" text-5xl py-2 text-teal-600 font-medium">
+          Samuel Pokam
+        </h2>
+        <h3 className=" text-2xl py-2">Frontend Developer</h3>
+        <p className="text-md py-5 leading-8 text-gray-800">
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+      </div>
+      <div className=" text-5xl flex justify-center gap-16 py-3 text-gray-600">
+        <AiFillLinkedin />
+        <AiFillGithub />
+        <AiFillMail />
+      </div>
+      <div className="relative mx-auto mt-20 bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden">
+        <Image src={deved} fill objectFit="cover" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
@@ -9,8 +11,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="bg-red-800">Hello React</h1>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <Navbar />
+          <Hero />
+        </section>
       </main>
     </>
   );
