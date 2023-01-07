8db24127195e0b16c2711d1515d45b37ef5b0b19
Samuel Pokam <dixiades@gmail.com>
2023-01-07 21:18:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
                 className="bg-gradient-to-r from-rose-600 to-orange-500 text-white px-4 py-2 rounded-lg ml-8 "
                 href="/resume.pdf"
               >
-                Resume
+                Resume or not
               </a>
             </li>
           </ul>
